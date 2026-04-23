cb51215197f91cb2686c6243d6fa0a0b690c3b64
Tiến Nguyễn <tiennguyen@10-141-47-99.wifi-m.deakin.edu.au>
2026-04-23 16:59:57 +1000

    fix bug signup, signin, login, forgot password and google account

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,11 +1,9 @@
-// src/supabaseClient.js
-import { createClient } from '@supabase/supabase-js';
-
+import { createClient } from "@supabase/supabase-js";
 const supabaseUrl =
   process.env.REACT_APP_SUPABASE_URL ||
-  'https://mdauzoueyzgtqsojttkp.supabase.co';
-const supabaseKey =
+  "https://mdauzoueyzgtqsojttkp.supabase.co";
+const supabaseAnonKey =
   process.env.REACT_APP_SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kYXV6b3VleXpndHFzb2p0dGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1MDE2MjYsImV4cCI6MjAyNzA3NzYyNn0.0EaAI8B563zQe9hcm4zjMWAlxCCYaw28mOXLcnRbooM';
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kYXV6b3VleXpndHFzb2p0dGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1MDE2MjYsImV4cCI6MjAyNzA3NzYyNn0.0EaAI8B563zQe9hcm4zjMWAlxCCYaw28mOXLcnRbooM";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
